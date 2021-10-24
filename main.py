6ac70321eed1ab1c8bf3c00c8351acbfd3105398
Wenng-Foon <92674500+Wenng-Foon@users.noreply.github.com>
2021-10-24 23:57:00 +1100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,83 @@
+import argparse
+import torch
+import torch.nn as nn
+import torchvision
+import torchvision.transforms as transforms
+import torchvision.models as models
+from torch.utils.data import DataLoader
+
+from model import Attention_ResNet50
+from train import train, resume, evaluate
+from recognition_dataloader import *
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(device)
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-id", "--exp_id", type=str, default='res_0')
+    parser.add_argument("-m", "--mode", choices=["resnet", "se", "cbam" ], default="none", help="To train a normal resnet or with SE/CBAM")
+    parser.add_argument("-b","--batch_size", type=int, default=16, help='number of training epochs')
+    parser.add_argument("-e","--epochs", type=int, default=50, help='number of training epochs')
+    parser.add_argument("-lr", "--learning_rate", type=float, default=1e-3, help='learning rate')
+    
+    parser.add_argument("-pre", "--pretrain", type=int, default=1, help='specify if pretraining is needed')
+    parser.add_argument("-red", "--reduction_ratio", type=int, default=16, help='reduction ratio for SE module')
+    parser.add_argument("-pool", "--pooling", choices=["avg", "max", "both" ], default="avg", help='specify pooling used by SE module')
+
+    parser.add_argument('--seed', type=int, default=1, help='random seed')
+    parser.add_argument('--resume', type=int, default=0, help='resume the trained model')
+    parser.add_argument('--test', type=int, default=0, help='test with trained model')
+
+    args = parser.parse_args()
+    return args
+
+if __name__ == '__main__':
+    args = parse_args()
+
+    torch.manual_seed(args.seed)
+    torch.cuda.manual_seed(args.seed)
+
+    # dataloaders
+    transform = transforms.Compose([transforms.ToTensor(),
+     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+
+    trainvalset = RecognitionImageFolder(root='./NUS', subdir='RecognitionData', transform=transform)
+    trainset, testset = torch.utils.data.random_split(trainvalset, [2250, 500])
+
+    trainloader = DataLoader(trainset, batch_size=args.batch_size,
+                                              shuffle=True, num_workers=2)
+    testloader = DataLoader(testset, batch_size=args.batch_size,
+                                              shuffle=False, num_workers=2)
+    dataloaders = (trainloader, testloader)
+
+    classes = ('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j')
+
+    # network
+    if args.pretrain == "0":
+        pretrained = False
+    else:
+        pretrained = True
+
+    if args.mode == "cbam":
+        model = Attention_ResNet50(pretrained=pretrained, reduction_ratio=args.reduction_ratio, pool_type=args.pool_type, use_Spatial= True).to(device)
+    elif args.mode == "se":
+        model = Attention_ResNet50(pretrained=pretrained, reduction_ratio=args.reduction_ratio, pool_type=args.pool_type, use_Spatial= False).to(device)
+    else:
+        model = models.resnet50(pretrained=pretrained)
+        model.fc = nn.Linear(2048,10)
+        model = model.to(device)
+
+    # optimizer
+    optimizer = torch.optim.Adam(model.parameters(), lr=args.learning_rate, betas=(0.9,0.999))
+
+    # resume the trained model
+    if args.resume:
+        model, optimizer = resume(args, model, optimizer)
+
+    if args.test == 1: # test mode, resume the trained model and test
+        testing_accuracy = evaluate(args, model, testloader)
+        print('testing finished, accuracy: {:.3f}'.format(testing_accuracy))
+    else: # train mode, train the network from scratch
+        train(args, model, optimizer, dataloaders)
+        print('training finished')
---
+++ recognition_dataloader.py
@@ -0,0 +1,67 @@
+import os
+from os.path import join
+import torch
+import torch.utils.data as data
+from torchvision.transforms import transforms
+from PIL import Image
+
+# Create a train folder in the root directory to hold the training data
+# Create subfolders for each label under the train folder according to the label name
+# Put the corresponding image data into the corresponding word folder.
+################################################
+# Set the directory structure
+# -NUS Hand Posture Dataset
+# ---train
+# -----1
+# -------g1.jpg
+# -------....
+# -----2
+# -------g2.jpg
+# -------....
+# -----3
+# -------g3.jpg
+# -------....
+# ...
+
+#Define image pre-processing (data enhancement) for training, test and validation data
+#We can modify the following parameters
+
+# test_transform = transforms.Compose([
+#     transforms.RandomHorizontalFlip(),
+#     transforms.Resize(45),
+#     transforms.CenterCrop(32),
+#     transforms.ToTensor(),
+#     transforms.Normalize(mean=[0.485, 0.456, 0.406],
+#                          std=[0.229, 0.224, 0.225])
+# ])
+
+#define suffix for finding and reading images
+#Had an error ValueError: num_samples should be a positive integer value, but got num_samples=0,
+#because the suffix is wrong, can't read the image
+#need to use () here, can't use [], can't be list data
+FileNameEnd = ('.jpeg', '.JPEG', '.tif', '.jpg', '.png', '.bmp')
+
+class RecognitionImageFolder(data.Dataset):
+    def __init__(self, root, subdir='RecognitionData', transform=None):
+        super(RecognitionImageFolder,self).__init__()
+
+        self.transform = transform  #Define the type of image conversion
+        self.image = []     #define the list to store images
+        self.img_dir = join(root, subdir)
+        self.label_dict = {'a':0, 'b':1, 'c':2, 'd':3, 'e':4, 'f':5, 'g':6, 'h':7, 'i':8, 'j':9}
+        
+    def __len__(self):
+        return len(os.listdir(self.img_dir))
+    
+    def __getitem__(self,idx):
+        name = os.listdir(self.img_dir)[idx]
+
+        label = self.label_dict[name[0]]
+
+        path = os.path.join(self.img_dir, name)
+        image = Image.open(path)
+        if self.transform:
+            image = self.transform(image)
+        return image,label
+
+
